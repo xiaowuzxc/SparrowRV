fad5a2a108125406e16a1c6fb9d6915b33c3fa73
xiaowuzxc <wuyan-100@163.com>
2022-07-05 17:01:45 +0800

    修复启动脚本

--- tb/run_zh.bat
+++ tb/run_zh.bat
@@ -1,23 +1,24 @@
 @chcp 65001
 :cmsl
 @echo ============================
-@echo input 0-9 or c to choice what you want.
+@echo 输入编号并回车，执行对应项目
 @echo ----------------------------
-@echo 0: load inst.txt and sim rtl
-@echo 1: regress ISA test
-@echo 2: .bin file turn into inst.txt
-@echo 3: load .bin file and sim rtl
-@echo 4: show last wave file
-@echo c: clean tb file
+@echo 0: 载入inst.txt并仿真 
+@echo 1: 执行RISC-V ISA测试集 
+@echo 2: bin文件转为inst.txt 
+@echo 3: 载入bin文件并仿真 
+@echo 4: 显示上一次的仿真波形
+@echo c: 清理缓存文件 
 @echo ============================
-@set /p cmchc = Enter number: 
+@set /p cmchc=输入命令编号：
 
 @if %cmchc% == 0 (python tools/isa_test.py sim_rtl & goto cmsl)^
 else if %cmchc% == 1 (python tools/isa_test.py all_isa & goto cmsl)^
 else if %cmchc% == 2 (python tools/isa_test.py tsr_bin & goto cmsl)^
 else if %cmchc% == 3 (python tools/isa_test.py sim_bin & goto cmsl)^
 else if %cmchc% == 4 (gtkwave tb.lxt & goto cmsl)^
-else if %cmchc% == c (del tb *.lxt inst.txt & @echo clean & goto cmsl)^
-else (echo Error 0 & goto cmsl)
+else if %cmchc% == c (del tb *.lxt inst.txt & @echo 缓存文件已清理 & goto cmsl)^
+else (echo Err 0: 命令未找到 & goto cmsl)
+
 
 pause
